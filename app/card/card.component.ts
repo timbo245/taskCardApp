bbb4f993f95d9015613cb22fa5a82404d981bdf5
timbo245 <tim@search.org>
2017-04-05 22:10:46 -0700

    initial card style

---
+++ app/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'appCard',
+    templateUrl: 'card.component.html',
+    styleUrls: [ 'card.component.css' ]
+})
+
+export class CardComponent { }
