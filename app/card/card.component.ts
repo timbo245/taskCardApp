c190a9fba47de24836f3bed7c2e0e048ee6e230d
timbo245 <tim@search.org>
2017-04-06 16:57:52 -0700

    added model folder

--- app/card/card.component.ts
+++ app/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Task } from '../model/task';
 
 @Component({
     moduleId: module.id,
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
     styleUrls: [ 'card.component.css' ]
 })
 
-export class CardComponent { }
+export class CardComponent { 
+    @Input() task: Task;
+}
